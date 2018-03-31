c2e7936d04c4e0999f9ce16f95de125f500d043c
Renan Freitas <rfl3@cin.ufpe.br>
2018-03-31 17:46:01 -0300

    this is a mess. help. but working tho...

--- teste.playground/Contents.swift
+++ teste.playground/Contents.swift
@@ -7,8 +7,59 @@ var maxTag = -1
 
 public class MyViewController : UIViewController {
     
+    // Defines the main texts for each dot
+    public let dotsTexts = [
+        "p1": """
+        ERA UMA VEZ uma linda menina que vivia no bosque e a quem todos chamavam, carinhosamente, de capuchinho vermelho.
+        
+        Um dia a mãe chamou-a e pediu-lhe um favor:
+        
+        - Coloquei neste cesto um bolo e um pote de mel. Leva-o à avozinha, que tem andado adoentada. Mas Capuchinho, tem cuidado! Não te desvies do teu caminho e não fales com desconhecidos.
+        
+        - Sim mãe, farei como dizes - prometeu Capuchinho Vermelho.
+        """,
+        "p2": "ALO?",
+        "p3": "",
+        "p4": ""
+    ]
+    
+    // Defines each option's text
+    let choices = [
+        "p1": [
+            "p2": "vou pela floresta",
+            "p3": "vou pela estrada mais longa",
+            "p4": "eoq"
+        ],
+        "p2": [:]
+    ]
+    
+    // Defines the coordinates of each dot
+    let coords = [
+        "p1": (10, 10),
+        "p2": (100,100),
+        "p3": (30, 140),
+        "p4": (50 ,100)
+    ]
+    
+    // Defines each dot's neighbor
+    let neighbors = [
+        "p1": ["p2","p3"],
+        "p2": ["p4"],
+        "p3": ["p4"],
+        "p4": []
+    ]
+    
+    
+    public var options = [String : [String : UIButton]]()
+    public var paths = [String : [String : CAShapeLayer]]()
+    public var dots = [String : UIView]()
+    public let textBox = UILabel()
+    public let upperView = UIView()
+    public let lowerView = UIView()
+    public let choosingBox = UIView()
+    
     public override func loadView() {
-
+        
         let view = UIView()
         view.backgroundColor = .white
         
@@ -18,56 +69,32 @@ public class MyViewController : UIViewController {
         let font = UIFont(name: "IndieFlower", size: 15)
         
         // Creates the view for the texts
-        let upperView = UIView(frame: CGRect(x: 0, y: 0, width: 640, height: 300))
-        upperView.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        self.upperView.frame = CGRect(x: 0, y: 0, width: 640, height: 300)
+        self.upperView.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
         
         // Creates the view for the graph
-        let lowerView = UIView(frame: CGRect(x: 0, y: 300, width: 640, height: 180))
-        lowerView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        self.lowerView.frame = CGRect(x: 0, y: 300, width: 640, height: 180)
+        self.lowerView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         
-        view.addSubview(upperView)
-        view.addSubview(lowerView)
+        view.addSubview(self.upperView)
+        view.addSubview(self.lowerView)
         
         // UPPER BODY
         
-        // Defines the texts template
-        let texts = [
-            "t1": """
-        ERA UMA VEZ uma linda menina que vivia no bosque e a quem todos chamavam, carinhosamente, de capuchinho vermelho.
+        self.textBox.frame = CGRect(x: 10, y: 10, width: 620, height: 220)
+        self.textBox.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        self.textBox.text = self.dotsTexts["p1"]!
+        self.textBox.textColor = .white
+        self.textBox.numberOfLines = 0
+        self.textBox.textAlignment = .left
+        self.textBox.font = font!
+        self.textBox.sizeToFit()
         
-        Um dia a mãe chamou-a e pediu-lhe um favor:
+        self.choosingBox.frame = CGRect(x: 10, y: 230, width: 620, height: 50)
+        self.choosingBox.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         
-        - Coloquei neste cesto um bolo e um pote de mel. Leva-o à avozinha, que tem andado adoentada. Mas Capuchinho, tem cuidado! Não te desvies do teu caminho e não fales com desconhecidos.
-        
-        - Sim mãe, farei como dizes - prometeu Capuchinho Vermelho.
-        """
-        ]
-        
-        
-        
-        let textBox = UILabel(frame: CGRect(x: 10, y: 10, width: 620, height: 220))
-        textBox.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
-        textBox.text = texts["t1"]!
-        textBox.textColor = .white
-        textBox.numberOfLines = 0
-        textBox.textAlignment = .left
-        textBox.font = font!
-        
-        
-        let choosingBox = UIView(frame: CGRect(x: 10, y: 230, width: 620, height: 50))
-        choosingBox.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
-        
-        let choices = [
-            "p1": [
-                "p2": "vou pela floresta",
-                "p3": "vou pela estrada mais longa",
-                "p4": "eoq"
-            ],
-            "p2": [:]
-        ]
-        
-        let op = createOptions(choices: choices, font: font!)
-        for i in op["p1"]! {
+        self.options = createOptions(choices: self.choices, font: font!)
+        for i in self.options["p1"]! {
             choosingBox.addSubview(i.value)
         }
         
@@ -76,52 +103,42 @@ public class MyViewController : UIViewController {
         
         
         // LOWER BODY
-        
-        // Text for each dot
-        let dotsText = [
-            "p1": texts["t1"]!,
-            "p2": "",
-            "p3": "",
-            "p4": ""
-        ]
-        
-        // Defines the coordinates of the dots dict
-        let coords = [
-            "p1": (10, 10),
-            "p2": (100,100),
-            "p3": (30, 140),
-            "p4": (50 ,100)
-        ]
 
         // Calls the function to create the UIView dict
-        let dots = createDotz(coords)
-
-        // Defines each dot's neighbor
-        let neighbors = [
-            "p1": ["p2","p3"],
-            "p2": [],
-            "p3": ["p4"],
-            "p4": []
-        ]
+        self.dots = createDotz(self.coords)
 
         // Calls the function to create all paths
-        let paths = createPaths(neighbors: neighbors, dots: dots)
+        self.paths = createPaths(neighbors: self.neighbors, dots: self.dots)
         
         // Teste criando todos os pontos e paths
-        for i in dots {
+        for i in self.dots {
             lowerView.addSubview(i.value)
         }
-        for i in paths {
+        for i in self.paths {
             for j in i.value {
                 lowerView.layer.addSublayer(j.value)
             }
         }
         
-        
         self.view = view
     }
     
-    // Creates the UILabel for the options
+    // Handles view changes
+    func changeView(_ dot : String) {
+        
+        UIView.animate(withDuration: 2, animations: {
+            self.textBox.alpha = 0.0
+        }, completion: {
+            (value: Bool) in
+            self.textBox.text = self.dotsTexts[dot]!
+            UIView.animate(withDuration: 2, animations: {
+                self.textBox.alpha = 1.0
+            })
+        })
+    }
+    
+    
+    // Creates the UIButtons for the options
     func createOptions(choices: [String : [String : String]], font: UIFont) -> [String : [String : UIButton]]{
         
         var result = [String : [String : UIButton]]()
@@ -157,6 +174,7 @@ public class MyViewController : UIViewController {
         return result
         
     }
+    
     // TODO - AJEITAR ESSA PORRA OS BOTAO E TAL
     // Action of the button
     @objc func nextText(sender : UIButton!) {
@@ -164,7 +182,7 @@ public class MyViewController : UIViewController {
         case 2:
             print("EOQ")
         default:
-            break
+            print("ALO")
         }
     }
     
@@ -176,7 +194,7 @@ public class MyViewController : UIViewController {
         
         for dot in list {
             let circulinho = UIView(frame: CGRect(x: dot.value.0, y: dot.value.1, width: 20, height: 20))
-            circulinho.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            circulinho.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
             circulinho.layer.cornerRadius = circulinho.frame.width / 2
             
             result[dot.key] = circulinho
@@ -203,7 +221,7 @@ public class MyViewController : UIViewController {
                     layer.path = path.cgPath
                     
                     layer.lineWidth = 4.0
-                    layer.strokeColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1).cgColor
+                    layer.strokeColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).cgColor
                     
                     if let _ = result[dot.key] {
                         result[dot.key]![neighbor] = layer
